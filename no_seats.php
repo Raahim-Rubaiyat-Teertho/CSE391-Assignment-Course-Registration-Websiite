5ae158d666361ffe4546e9fbfff907691ccaf683
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2023-10-27 20:41:11 +0600

    no seats in the registration page handled

---
+++ no_seats.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No seats remaining</title>
+</head>
+<body>
+    <h1>This section does not have anymore seats. Please choose another section.</h1>
+</body>
+</html>
